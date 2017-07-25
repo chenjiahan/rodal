083703ff50c7587038eb7499a51644eea82f5d67
陈嘉涵 <chenjiahan@meituan.com>
2017-07-25 19:38:07 +0800

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,8 @@ module.exports = {
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
             }
-        })
+        }),
+        new webpack.optimize.ModuleConcatenationPlugin()
     ],
     devServer: {
         contentBase: './',
