bb53ffb5164c6b9f9dfb8201ec565bdac7c26858
陈嘉涵 <chenjiahan@youzan.com>
2020-02-04 15:55:14 +0800

    chore: bump babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -2,56 +2,40 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        bundle: './docs/src/index.js'   
-    },
-    output: {
-        path: path.resolve(__dirname, 'docs/dist'),
-        publicPath: '/docs/dist/',
-        filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['es2015', 'stage-0', 'react']
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.ModuleConcatenationPlugin()
-    ],
-    devServer: {
-        contentBase: './',
-        compress: true,
-        port: 2345,
-        stats: {
-            assets: true,
-            children: false,
-            chunks: false,
-            hash: false,
-            modules: false,
-            publicPath: false,
-            timings: false,
-            version: false,
-            warnings: true,
-            colors: {
-                green: '\u001b[32m',
-            }
+  entry: {
+    bundle: './docs/src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'docs/dist'),
+    publicPath: '/docs/dist/',
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
         }
-    }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin()
+  ],
+  devServer: {
+    contentBase: './docs',
+    port: 2345,
+    compress: true,
+    stats: "errors-only"
+  }
 };
