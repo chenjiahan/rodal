1ede73894d800d7e60b0946d928dfb6d492a1adb
陈嘉涵 <chenjiahan@youzan.com>
2020-02-04 15:44:15 +0800

    chore: prettify code

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,75 +1,88 @@
-import React    from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import Rodal    from '../../src/rodal';
+import Rodal from '../../src/rodal';
 import './index.css';
 import '../../src/rodal.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor (props) {
-        super(props);
+    this.state = {
+      visible: false,
+      animation: 'zoom'
+    };
+  }
 
-        this.state = {
-            visible: false,
-            animation: 'zoom'
-        }
-    }
+  show = animation => {
+    this.setState({
+      animation,
+      visible: true
+    });
+  };
 
-    show = (animation) => {
-        this.setState({
-            animation,
-            visible: true
-        });
-    }
+  hide = () => {
+    this.setState({ visible: false });
+  };
 
-    hide = () => {
-        this.setState({ visible: false });
-    }
+  render() {
+    const { visible, animation } = this.state;
+    const types = [
+      'zoom',
+      'fade',
+      'flip',
+      'door',
+      'rotate',
+      'slideUp',
+      'slideDown',
+      'slideLeft',
+      'slideRight'
+    ];
+    const buttons = types.map((value, index) => {
+      const style = {
+        animationDelay: index * 100 + 'ms',
+        WebkitAnimationDelay: index * 100 + 'ms'
+      };
+      return (
+        <button
+          key={index}
+          className="btn scale"
+          onClick={this.show.bind(this, value)}
+          style={style}
+        >
+          {value}
+        </button>
+      );
+    });
 
-    onKeyPressHide = (event) => {
-        if (event.which === 13) {
-            this.hide();
-        }
-    }
-
-    render () {
-        const { visible, animation } = this.state;
-        const types = ['zoom', 'fade', 'flip', 'door', 'rotate', 'slideUp', 'slideDown', 'slideLeft', 'slideRight'];
-        const buttons = types.map((value, index) => {
-            const style = {
-                animationDelay       : index * 100 + 'ms',
-                WebkitAnimationDelay : index * 100 + 'ms'
-            };
-            return (
-                <button key={index} className="btn scale" onClick={this.show.bind(this, value)} style={style}>
-                    {value}
-                </button>
-            )
-        });
-
-        return (
-            <div className="wrap">
-                <div className="container" style={{ paddingTop: (window.innerHeight - 440) / 2 }}>
-                    <h1 className="title scale">Rodal</h1>
-                    <h3 className="intro scale">A React modal with animations.</h3>
-                    <div className="btn-area">{buttons}</div>
-                </div>
-                <Rodal visible={visible}
-                       onClose={this.hide}
-                       animation={animation}
-                       closeOnEsc
-                >
-                    <div className="header">Rodal</div>
-                    <div className="body">A React modal with animations.</div>
-                    <button className="rodal-confirm-btn" onClick={this.hide}>ok</button>
-                    <button className="rodal-cancel-btn" onClick={this.hide}>close</button>
-                </Rodal>
-            </div>
-        )
-    }
+    return (
+      <div className="wrap">
+        <div
+          className="container"
+          style={{ paddingTop: (window.innerHeight - 440) / 2 }}
+        >
+          <h1 className="title scale">Rodal</h1>
+          <h3 className="intro scale">A React modal with animations.</h3>
+          <div className="btn-area">{buttons}</div>
+        </div>
+        <Rodal
+          visible={visible}
+          onClose={this.hide}
+          animation={animation}
+          closeOnEsc
+        >
+          <div className="header">Rodal</div>
+          <div className="body">A React modal with animations.</div>
+          <button className="rodal-confirm-btn" onClick={this.hide}>
+            ok
+          </button>
+          <button className="rodal-cancel-btn" onClick={this.hide}>
+            close
+          </button>
+        </Rodal>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
--- src/rodal.js
+++ src/rodal.js
@@ -12,164 +12,181 @@ const UA = IN_BROWSER && window.navigator.userAgent.toLowerCase();
 const IS_IE_9 = UA && UA.indexOf('msie 9.0') > 0;
 
 const Dialog = props => {
-    const animation = (props.animationType === 'enter' ? props.enterAnimation : props.leaveAnimation) || props.animation;
-    const className = `rodal-dialog rodal-${animation}-${props.animationType}`;
-    const CloseButton = props.showCloseButton ? <span className="rodal-close" onClick={props.onClose} onKeyPress={(event) => {
-        if (props.onClose && event.which === 13) {
-            props.onClose();
-        }
-    }} tabIndex={0} /> : null;
-    const { width, height, measure, duration, customStyles } = props;
-    const style = {
-        width: width + measure,
-        height: height + measure,
-        animationDuration: duration + 'ms',
-        WebkitAnimationDuration: duration + 'ms'
-    };
-    const mergedStyles = { ...style, ...customStyles };
+  const animation =
+    (props.animationType === 'enter'
+      ? props.enterAnimation
+      : props.leaveAnimation) || props.animation;
 
-    return (
-        <div style={mergedStyles} className={className}>
-            {props.children}
-            {CloseButton}
-        </div>
-    )
+  const className = `rodal-dialog rodal-${animation}-${props.animationType}`;
+
+  const CloseButton = props.showCloseButton ? (
+    <span
+      className="rodal-close"
+      onClick={props.onClose}
+      onKeyPress={event => {
+        if (props.onClose && event.which === 13) {
+          props.onClose();
+        }
+      }}
+      tabIndex={0}
+    />
+  ) : null;
+
+  const { width, height, measure, duration, customStyles } = props;
+
+  const style = {
+    width: width + measure,
+    height: height + measure,
+    animationDuration: duration + 'ms',
+    WebkitAnimationDuration: duration + 'ms'
+  };
+
+  const mergedStyles = { ...style, ...customStyles };
+
+  return (
+    <div style={mergedStyles} className={className}>
+      {props.children}
+      {CloseButton}
+    </div>
+  );
 };
 
 class Rodal extends React.Component {
+  static propTypes = {
+    width: PropTypes.number,
+    height: PropTypes.number,
+    measure: PropTypes.string,
+    visible: PropTypes.bool,
+    showMask: PropTypes.bool,
+    closeOnEsc: PropTypes.bool,
+    closeMaskOnClick: PropTypes.bool,
+    showCloseButton: PropTypes.bool,
+    animation: PropTypes.string,
+    enterAnimation: PropTypes.string,
+    leaveAnimation: PropTypes.string,
+    duration: PropTypes.number,
+    className: PropTypes.string,
+    customStyles: PropTypes.object,
+    customMaskStyles: PropTypes.object,
+    onClose: PropTypes.func.isRequired,
+    onAnimationEnd: PropTypes.func
+  };
 
-    static propTypes = {
-        width            : PropTypes.number,
-        height           : PropTypes.number,
-        measure          : PropTypes.string,
-        visible          : PropTypes.bool,
-        showMask         : PropTypes.bool,
-        closeOnEsc       : PropTypes.bool,
-        closeMaskOnClick : PropTypes.bool,
-        showCloseButton  : PropTypes.bool,
-        animation        : PropTypes.string,
-        enterAnimation   : PropTypes.string,
-        leaveAnimation   : PropTypes.string,
-        duration         : PropTypes.number,
-        className        : PropTypes.string,
-        customStyles     : PropTypes.object,
-        customMaskStyles : PropTypes.object,
-        onClose          : PropTypes.func.isRequired,
-        onAnimationEnd   : PropTypes.func
+  static defaultProps = {
+    width: 400,
+    height: 240,
+    measure: 'px',
+    visible: false,
+    showMask: true,
+    closeOnEsc: false,
+    closeMaskOnClick: true,
+    showCloseButton: true,
+    animation: 'zoom',
+    enterAnimation: '',
+    leaveAnimation: '',
+    duration: 300,
+    className: '',
+    customStyles: {},
+    customMaskStyles: {}
+  };
+
+  state = {
+    isShow: false,
+    animationType: 'leave'
+  };
+
+  componentDidMount() {
+    if (this.props.visible) {
+      this.enter();
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.visible && !prevProps.visible) {
+      this.enter();
+    }
+
+    if (!this.props.visible && prevProps.visible) {
+      this.leave();
+    }
+  }
+
+  enter() {
+    this.setState({ isShow: true, animationType: 'enter' });
+  }
+
+  leave() {
+    this.setState(IS_IE_9 ? { isShow: false } : { animationType: 'leave' });
+  }
+
+  onKeyUp = event => {
+    if (!this.props.closeOnEsc || event.keyCode !== 27) {
+      return;
+    }
+
+    this.props.onClose();
+  };
+
+  animationEnd = event => {
+    const { animationType } = this.state;
+    const { closeOnEsc, onAnimationEnd } = this.props;
+
+    if (animationType === 'leave') {
+      this.setState({ isShow: false });
+    } else if (closeOnEsc) {
+      this.el.focus();
+    }
+
+    if (event.target === this.el && onAnimationEnd) {
+      onAnimationEnd();
+    }
+  };
+
+  render() {
+    const {
+      closeMaskOnClick,
+      onClose,
+      customMaskStyles,
+      showMask,
+      duration,
+      className,
+      children
+    } = this.props;
+
+    const { isShow, animationType } = this.state;
+
+    const Mask = showMask ? (
+      <div
+        className="rodal-mask"
+        style={customMaskStyles}
+        onClick={closeMaskOnClick ? onClose : void 0}
+      />
+    ) : null;
+
+    const style = {
+      display: isShow ? '' : 'none',
+      animationDuration: duration + 'ms',
+      WebkitAnimationDuration: duration + 'ms'
     };
 
-    static defaultProps = {
-        width            : 400,
-        height           : 240,
-        measure          : 'px',
-        visible          : false,
-        showMask         : true,
-        closeOnEsc       : false,
-        closeMaskOnClick : true,
-        showCloseButton  : true,
-        animation        : 'zoom',
-        enterAnimation   : '',
-        leaveAnimation   : '',
-        duration         : 300,
-        className        : '',
-        customStyles     : {},
-        customMaskStyles : {},
-    };
-
-    state = {
-        isShow: false,
-        animationType: 'leave'
-    };
-
-    componentDidMount() {
-        if (this.props.visible) {
-            this.enter();
-        }
-    }
-
-    componentDidUpdate(prevProps) {
-      if (this.props.visible && !prevProps.visible) {
-        this.enter();
-      }
-
-      if (!this.props.visible && prevProps.visible) {
-        this.leave();
-      }
-    }
-
-    enter() {
-        this.setState({ isShow: true, animationType: 'enter' });
-    }
-
-    leave() {
-        this.setState(IS_IE_9 ? { isShow: false } : { animationType: 'leave' });
-    }
-
-    onKeyUp = event => {
-        if (!this.props.closeOnEsc || event.keyCode !== 27) {
-          return;
-        }
-
-        this.props.onClose();
-    }
-
-    animationEnd = event => {
-        const { animationType } = this.state;
-        const { closeOnEsc, onAnimationEnd } = this.props;
-
-        if (animationType === 'leave') {
-            this.setState({ isShow: false });
-        } else if (closeOnEsc) {
-            this.el.focus();
-        }
-
-        if (event.target === this.el && onAnimationEnd) {
-          onAnimationEnd();
-        }
-    }
-
-    render() {
-        const {
-          closeMaskOnClick,
-          onClose,
-          customMaskStyles,
-          showMask,
-          duration,
-          className,
-          children
-        } = this.props;
-        const { isShow, animationType } = this.state;
-        const mask = showMask
-          ? (
-            <div
-              className="rodal-mask"
-              style={customMaskStyles}
-              onClick={closeMaskOnClick ? onClose : void 0}
-            />
-          )
-          : null;
-        const style = {
-            display: isShow ? '' : 'none',
-            animationDuration: duration + 'ms',
-            WebkitAnimationDuration: duration + 'ms'
-        };
-
-        return (
-            <div
-                style={style}
-                className={cx('rodal', `rodal-fade-${animationType}`, className)}
-                onAnimationEnd={this.animationEnd}
-                tabIndex="-1"
-                ref={el => { this.el = el; }}
-                onKeyUp={this.onKeyUp}
-            >
-                {mask}
-                <Dialog {...this.props} animationType={animationType}>
-                    {children}
-                </Dialog>
-            </div>
-        )
-    }
+    return (
+      <div
+        style={style}
+        className={cx('rodal', `rodal-fade-${animationType}`, className)}
+        onAnimationEnd={this.animationEnd}
+        tabIndex="-1"
+        ref={el => {
+          this.el = el;
+        }}
+        onKeyUp={this.onKeyUp}
+      >
+        {Mask}
+        <Dialog {...this.props} animationType={animationType}>
+          {children}
+        </Dialog>
+      </div>
+    );
+  }
 }
 
 export default Rodal;
