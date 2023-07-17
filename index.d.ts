24257ad030b270313731e76039164302672beed8
chenjiahan <chenjiahan.jait@bytedance.com>
2023-07-17 08:48:22 +0800

    chore: add types field to package.json

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,26 @@
 declare module 'rodal' {
-    import {MouseEventHandler, JSX, PropsWithChildren} from 'react';
+  import { MouseEventHandler, JSX, PropsWithChildren } from 'react';
 
-    type RodalProps = PropsWithChildren & {
-        width?: number;
-        height?: number;
-        measure?: string;
-        visible?: boolean;
-        showMask?: boolean;
-        closeOnEsc?: boolean;
-        closeMaskOnClick?: boolean;
-        showCloseButton?: boolean;
-        animation?: string;
-        enterAnimation?: string;
-        leaveAnimation?: string;
-        duration?: number;
-        className?: string;
-        customStyles?: Record<string, any>;
-        customMaskStyles?: Record<string, any>;
-        onClose?: MouseEventHandler<HTMLSpanElement>;
-        onAnimationEnd?: () => never;
-    };
+  type RodalProps = PropsWithChildren & {
+    width?: number;
+    height?: number;
+    measure?: string;
+    visible?: boolean;
+    showMask?: boolean;
+    closeOnEsc?: boolean;
+    closeMaskOnClick?: boolean;
+    showCloseButton?: boolean;
+    animation?: string;
+    enterAnimation?: string;
+    leaveAnimation?: string;
+    duration?: number;
+    className?: string;
+    customStyles?: Record<string, any>;
+    customMaskStyles?: Record<string, any>;
+    onClose?: MouseEventHandler<HTMLSpanElement>;
+    onAnimationEnd?: () => never;
+  };
 
-    function Rodal(_: RodalProps): JSX.Element;
-    export = Rodal;
+  function Rodal(_: RodalProps): JSX.Element;
+  export = Rodal;
 }
